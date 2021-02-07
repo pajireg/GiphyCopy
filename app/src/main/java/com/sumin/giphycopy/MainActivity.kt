cc61722adb35ed9d49437f2bb020a83b7248da70
csm <pajireg@gmail.com>
2021-02-07 14:40:57 +0900

    Initial commit

---
+++ app/src/main/java/com/sumin/giphycopy/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.sumin.giphycopy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
